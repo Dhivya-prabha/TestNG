618c5efbd3016fe75da9e1b00548e3e6902b57ac
Dhivya-prabha <49254873+Dhivya-prabha@users.noreply.github.com>
2025-08-01 16:44:13 +0530

    Retry.java

--- Leads/Retry.java
+++ Leads/Retry.java
@@ -1,24 +1,24 @@
 package Leads;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
 
 import org.testng.IAnnotationTransformer;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
-import org.testng.annotations.ITestAnnotation;
 
-public class Retry implements IRetryAnalyzer {
-	int maxtry = 2, iTries = 0;
-
-	public boolean retry(ITestResult result) {
-		// TODO Auto-generated method stub
-		if(iTries < maxtry) {
-			iTries++;
-		}
-		return false;
-		
-	}
-
+public class RetryFailedTest implements IRetryAnalyzer{
+	private static final int max = 2;
+	private int retryCount = 0;
 	
+	
+	public boolean retry(ITestResult result) {
+		if(retryCount < max) {
+			retryCount++;
+			System.out.println("Max Count: " + retryCount);
+			return true;
+		}
+		
+		return false;
+	}
+	
+
 }
